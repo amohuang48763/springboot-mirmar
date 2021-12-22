9fa1afbc1afb726000b1b6f9613e6d7bf1423500
amohuang <amohuang48763@gmail.com>
2021-12-22 19:59:55 +0800

    測試

--- src/main/java/com/demo/miramar/result/ResultCommon.java
+++ src/main/java/com/demo/miramar/result/ResultCommon.java
@@ -7,7 +7,7 @@ public class ResultCommon {
 	
 	public static JsonObject setResponseCode(JsonObject jsonObject,int code) {
 		jsonObject.addProperty("result", code);
-		
+		System.out.println("test");
 		return jsonObject;
 	}
 	
